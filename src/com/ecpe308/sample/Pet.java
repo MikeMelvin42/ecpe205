2099e7f42672439199d6abf38d5427c7dea7016f
MARIANE GRACE LOBRIDO GUMBAN <2301539@cc.usls.edu>
2025-04-30 15:00:20 +0800

    added a new attribute called age. made a getter/accessor and a setter/mutator for it

--- src/com/ecpe308/sample/Pet.java
+++ src/com/ecpe308/sample/Pet.java
@@ -3,6 +3,9 @@ package com.ecpe308.sample;
 public class Pet {
     public String name, breed; //persian, labrador, golden retriever
     public String pet_type;  //cat/dog
+    public String age;
+    
+    //but the getters are the accessors
 
     public String getName(){
         return name;
@@ -16,6 +19,12 @@ public class Pet {
         return pet_type;
     }
 
+    public String getAge() {
+        return age;
+    }
+
+    //these are the mutators
+
     public void setName (String name){
         this.name = name;
     }
@@ -26,6 +35,10 @@ public class Pet {
     
     public void setPet_type (String pet_type){
         this.pet_type= pet_type;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
     }
 }
 
